401596c7e60c4c325d6d4c9dd112e0e4cd8585f2
Sahar <saharabdelhaq02@gmail.com>
2022-09-21 14:59:09 +0300

    last commit

--- TCP/src/TCPClient.java
+++ TCP/src/TCPClient.java
@@ -7,30 +7,25 @@ public class TCPClient  {
         String sentence; 
         String modifiedSentence;
         
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
         while(true) {
         Socket clientSocket = new Socket("192.168.1.52", 9000); 
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
 
         BufferedReader inFromServer = 
                 new BufferedReader(new
                 InputStreamReader(clientSocket.getInputStream())); 
 
-              
-            	  System.out.println("Test sending a request from user ");
-            	  sentence = inFromUser.readLine();
-
+              sentence = inFromUser.readLine();
               outToServer.writeBytes(sentence+"\n"); 
-
               modifiedSentence = inFromServer.readLine(); 
               if(sentence.compareTo("q")==0) {
         		  System.out.println("End system");
         		  break;
     		  }
               System.out.println("FROM SERVER: " + modifiedSentence); 
-              clientSocket.close(); }             
+              clientSocket.close();
+              }             
           } 
       } 
--- TCP/src/TCPServer.java
+++ TCP/src/TCPServer.java
@@ -7,33 +7,40 @@ class TCPServer {
       String clientRequest; 
       String result; 
       ServerSocket welcomeSocket = new ServerSocket(9000); 
+      
       while(true) { 
-  
             Socket connectionSocket = welcomeSocket.accept(); 
-           BufferedReader inFromClient = 
-              new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
-           DataOutputStream  outToClient = 
-             new DataOutputStream(connectionSocket.getOutputStream()); 
+           BufferedReader inFromClient =  new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+           DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
 
            clientRequest = inFromClient.readLine(); 
            result = findinTCP(clientRequest);
            outToClient.writeBytes(result+'\n'); 
         } 
     }
-private static String findinTCP(String clientRequest) throws Exception {
+  
+   private static String findinTCP(String clientRequest) throws Exception {
 	  File TCP = new File("TCP.txt");
       Scanner analyzier = new Scanner(TCP);
       String response = "Error 404";
+      
 	 while (analyzier.hasNextLine()) {
 	        String data = analyzier.nextLine();
             String a[]= data.split(" ");
             if(a[0].equalsIgnoreCase(clientRequest.trim())) {
+            	
             	if(a[1].length()==1) {
-            		response=a[1]+" "+a[2] ;break;
-            	}else {
-            	response=a[1] ;break;}}
+            		response=a[1]+" "+a[2];
+            		break;
+            	}
+            	
+            	else {
+            	response=a[1];
+            	break;
+            	}
+            }
 	      }
-	return response;} 
+	return response;
+	} 
 } 
  
